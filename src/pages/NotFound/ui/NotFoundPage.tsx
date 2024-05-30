f26eaf32b7bfdfec29d39895e3e52d6918209f15
yurwow <yura_kuzin_03@mail.ru>
2024-05-30 19:41:12 +0500

    change NotFoundPages

--- src/pages/NotFound/ui/NotFoundPage.tsx
+++ src/pages/NotFound/ui/NotFoundPage.tsx
@@ -1,5 +1,4 @@
 import { Stack, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
 import { ErrorPageIcon } from '@/shared/ui/ErrorPageIcon/ErrorPageIcon.tsx';
 
 const NotFoundPage = () => (
@@ -10,10 +9,10 @@ const NotFoundPage = () => (
         height="100%"
     >
         <ErrorPageIcon />
-        <Typography variant="body2" maxWidth="50%" textAlign="center">
+        <Typography color="secondary.light" variant="body2" maxWidth="50%" textAlign="center">
             Извините, мы не можем найти эту страницу. Но не волнуйтесь, на нашей
             {' '}
-            <Link to="/">главной</Link>
+            главной
             {' '}
             странице вы найдете множество других вещей.
         </Typography>
